284cc7e5e0a868be46975f8793c7405d2d2893f8
Yoichi Kurokawa <yoichi.kk@gmail.com>
2019-01-06 12:44:06 +0900

    Initial commit

---
+++ sample.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template,request
+
+app =Flask(__name__)
+
+@app.route('/')
+def sample():
+	name = '株式会社キカガク'
+	return render_template('sample.html', name = name)
+
+@app.route('/sample2' , methods=["POST"])
+def sample2():
+	a = int(request.form['a'])
+	b = int(request.form['b'])
+	return render_template("sample2.html", a = a, b = b)
+
+
+if __name__ =='__main__':
+	app.run(debug=True)	
